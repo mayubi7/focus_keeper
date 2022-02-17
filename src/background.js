9b71d2ec516dce99d06d970d3faa66122111013f
Mariam <amaryam215@gmail.com>
2022-02-16 19:14:04 -0800

    basic timer functionality added

--- src/background.js
+++ src/background.js
@@ -1,10 +1,12 @@
 console.log("background running");
+var start = new Date();
 var focusing = false;
 
 chrome.runtime.onMessage.addListener(function(text) {
     if (text === "started session") {
         focusing = true;
         console.log(text);
+        timer();
     } else if (text === "end session") {
         focusing = false;
         console.log(text);
@@ -15,4 +17,23 @@ chrome.tabs.onUpdated.addListener( function(tab) {
         chrome.tabs.query({active: true, currentWindow: true},
         function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, "continue session");});};})
+
+function timer() {
+    start = new Date();
+    var sec = 30;
+    var timer = setInterval(function () {
+    var currentSecondsPassed = ((new Date).getTime() - start) / 1000;
+    console.log(currentSecondsPassed);
+    if (sec - currentSecondsPassed < 0) {
+        clearInterval(timer);
+        chrome.tabs.query({}, function (tabs) {
+                 tabs.forEach(function (tab) {
+                     chrome.tabs.sendMessage(tab.id, "time is up");
+                 });
+            });
+        focusing = false;
+        }
+    }, 1000);
+}
+
 
--- src/content.js
+++ src/content.js
@@ -3,16 +3,21 @@ var current = window.location.href;
 chrome.runtime.onMessage.addListener(function (request) {
     if (request === "end session") {
         window.location.reload(false);
-    } else if(toBlock()) {
+    } else if (request === "time is up") {
+        window.location.reload(false);
+        alert("Great Job! Focus session completed!");
+    } else if (toBlock()) {
         document.documentElement.innerHTML = '';
         document.documentElement.innerHTML = 'Domain is blocked';
         document.documentElement.scrollTop = 0;}
         });
-//make a list of strings and iterate over that by doing includes?
+
 function toBlock() {
     return current.includes("www.facebook.com") ||
     current.includes("mail.google.com") ||
     current.includes("www.instagram.com") ||
     current.includes("youtube.com") ||
     current.includes("www.pinterest.com");}
+
+
 
